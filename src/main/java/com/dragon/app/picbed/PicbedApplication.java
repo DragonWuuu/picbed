144fc25b1f38c6f909c8d25598d8b25274dd7c2e
DragonWuuu <3350986682@qq.com>
2025-09-07 01:33:38 +0800

    build:init spring boot

---
+++ src/main/java/com/dragon/app/picbed/PicbedApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.app.picbed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicbedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicbedApplication.class, args);
+	}
+
+}
